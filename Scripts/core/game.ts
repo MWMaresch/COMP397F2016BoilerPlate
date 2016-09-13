954402d417f998eea904c712ff0d1d17c809d72f
mmaresch <300749466@cencol.ca>
2016-09-13 10:39:10 -0400

    initial commit

---
+++ Scripts/core/game.ts
@@ -0,0 +1,33 @@
+console.log("game is starting");
+
+class Person {
+    protected _name : string;
+
+    constructor(name:string) {
+        this._name = name;
+    }
+
+    public speak() : void {
+        console.log(this._name + " says hi");
+    }
+}
+
+class Student extends Person {
+    private _studentNum : number;
+
+    constructor(name:string, studentNum:number){
+        super(name);
+        this._studentNum = studentNum;
+    }
+
+    public studies() : void {
+        console.log(this._name + " is studying");
+    }
+}
+
+var person : Person = new Person("Qwert");
+person.speak();
+
+var student : Student = new Student("Donald", 1234);
+student.speak();
+student.studies();
