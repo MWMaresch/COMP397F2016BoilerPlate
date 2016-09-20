6ed15b05d35946644ab3ba1f2e9236686c720ce7
MWMaresch <michael@maresch.co>
2016-09-19 21:27:38 -0400

    Added smiley face
    Lab 2

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,5 +1,9 @@
 var canvas;
 var stage;
+var circle;
+var eyeL;
+var eyeR;
+var mouth;
 function init() {
     canvas = document.getElementById("canvas");
     stage = new createjs.Stage(canvas);
@@ -8,7 +12,30 @@ function init() {
     createjs.Ticker.on("tick", gameLoop, this);
 }
 function gameLoop(event) {
-    var myNewText = new createjs.Text("Test", "60px Consolas", "#000000");
+    var myNewText = new createjs.Text(" AYYYYYYY", "60px Consolas", "#EDEDED");
+    circle = new createjs.Shape();
+    eyeL = new createjs.Shape();
+    eyeR = new createjs.Shape();
+    mouth = new createjs.Shape();
+    circle.graphics.beginFill("yellow").drawCircle(0, 0, 80);
+    eyeL.graphics.beginFill("black").drawCircle(0, 0, 10);
+    eyeR.graphics.beginFill("black").drawCircle(0, 0, 10);
+    mouth.graphics.beginFill("black").arc(0, 0, 40, 2.5, 0.7, true);
+    //mouth.graphics.beginFill("black").arc()
+    //Set position of Shape instance.
+    circle.x = canvas.clientWidth / 2;
+    circle.y = 200;
+    eyeL.x = circle.x - 40;
+    eyeR.x = circle.x + 40;
+    eyeL.y = circle.y;
+    eyeR.y = circle.y;
+    mouth.x = circle.x;
+    mouth.y = circle.y + 4;
+    //Add Shape instance to stage display list.
+    stage.addChild(circle);
+    stage.addChild(eyeL);
+    stage.addChild(eyeR);
+    stage.addChild(mouth);
     stage.addChild(myNewText);
     stage.update();
 }
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,7 +1,13 @@
 var canvas : HTMLElement;
 var stage : createjs.Stage;
+var circle : createjs.Shape;
+var eyeL : createjs.Shape;
+var eyeR : createjs.Shape;
+var mouth : createjs.Shape;
+
 
 function init() {
+
     canvas = document.getElementById("canvas");
     stage = new createjs.Stage(canvas);
     stage.enableMouseOver(20);
@@ -11,9 +17,31 @@ function init() {
 
 function gameLoop(event : createjs.TickerEvent) : void
 {
-    var myNewText : createjs.Text = new createjs.Text("Test", "60px Consolas", "#000000");
-
+    var myNewText : createjs.Text = new createjs.Text(" AYYYYYYY", "60px Consolas", "#EDEDED");
+    circle = new createjs.Shape();
+    eyeL = new createjs.Shape();
+    eyeR = new createjs.Shape();
+    mouth = new createjs.Shape();
+    circle.graphics.beginFill("yellow").drawCircle(0, 0, 80);
+    eyeL.graphics.beginFill("black").drawCircle(0, 0, 10);
+    eyeR.graphics.beginFill("black").drawCircle(0, 0, 10);
+    mouth.graphics.beginFill("black").arc(0,0,40,2.5,0.7,true);
+    //mouth.graphics.beginFill("black").arc()
+    
+    //Set position of Shape instance.
+    circle.x = canvas.clientWidth / 2;
+    circle.y = 200;
+    eyeL.x = circle.x - 40;
+    eyeR.x = circle.x + 40;
+    eyeL.y = circle.y;
+    eyeR.y = circle.y;
+    mouth.x = circle.x;
+    mouth.y = circle.y + 4;
+    //Add Shape instance to stage display list.
+    stage.addChild(circle);
+    stage.addChild(eyeL);
+    stage.addChild(eyeR);
+    stage.addChild(mouth);
     stage.addChild(myNewText);
-
     stage.update();
 }
